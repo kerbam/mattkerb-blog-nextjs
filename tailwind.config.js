eb88bb6f5a1d22acbf5d2914eb6b310843359216
Matt Kerbawy <matt@pendo.io>
2022-01-25 20:37:18 -0500

    remove purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js', './lib/**/*.js'],
   darkMode: 'class',
   theme: {
     extend: {
